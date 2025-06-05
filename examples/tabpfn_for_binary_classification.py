76da0742064bcb2122e577dd6819afb5012f89fa
Ang Ming Ze, Aaron <112754466+Auradraco@users.noreply.github.com>
2025-06-05 17:30:40 +0800

    Included log_loss for performance metric analysis

--- examples/tabpfn_for_binary_classification.py
+++ examples/tabpfn_for_binary_classification.py
@@ -6,7 +6,7 @@ using the breast cancer dataset from scikit-learn.
 """
 from jinja2 import Template
 from sklearn.datasets import load_breast_cancer
-from sklearn.metrics import accuracy_score, roc_auc_score, f1_score, confusion_matrix, classification_report
+from sklearn.metrics import accuracy_score, roc_auc_score, f1_score, confusion_matrix, classification_report, log_loss
 from sklearn.model_selection import train_test_split
 from tabpfn import TabPFNClassifier
 import pandas as pd
@@ -40,6 +40,9 @@ clf.fit(X_train, y_train)
 # Predict probabilities
 prediction_probabilities = clf.predict_proba(X_test)
 print("ROC AUC:", roc_auc_score(y_test, prediction_probabilities[:, 1]))
+print("Log Loss: ", log_loss(y_test, prediction_probabilities))
+
+
 
 # Predict labels
 predictions = clf.predict(X_test)
@@ -53,66 +56,67 @@ predictions = clf.predict(X_test)
 print("F1 Score", f1_score(y_test, predictions))
 
 
-confusion = confusion_matrix(y_test, predictions, labels=clf.classes_)
-plt.figure(figsize=(10, 8))
 
-sns.heatmap(confusion, 
-            annot=True, 
-            fmt='d', 
-            cmap= 'flare',
-            xticklabels=["Malignant", "Benign"],
-            yticklabels=["Malignant", "Benign"],
-            annot_kws={"size": 35, "color": "black"}
-            )
-plt.xlabel('Predicted label', fontsize=20)
-plt.ylabel('True label', fontsize=20)
-plt.title('Confusion Matrix of Breast Cancer Classification', fontsize=24)
-# plt.show()
+# confusion = confusion_matrix(y_test, predictions, labels=clf.classes_)
+# plt.figure(figsize=(10, 8))
+
+# sns.heatmap(confusion, 
+#             annot=True, 
+#             fmt='d', 
+#             cmap= 'flare',
+#             xticklabels=["Malignant", "Benign"],
+#             yticklabels=["Malignant", "Benign"],
+#             annot_kws={"size": 35, "color": "black"}
+#             )
+# plt.xlabel('Predicted label', fontsize=20)
+# plt.ylabel('True label', fontsize=20)
+# plt.title('Confusion Matrix of Breast Cancer Classification', fontsize=24)
+# # plt.show()
 
 
-savedir = "results/"
-confusion_filename = "confusion_matrix_breast_cancer.png"
-confusion_path = os.path.join(savedir, confusion_filename)
-plt.savefig(confusion_path)
-plt.close()
-# print(f"Confusion Matrix: \n{confusion}")
+# savedir = "results/"
+# confusion_filename = "confusion_matrix_breast_cancer.png"
+# confusion_path = os.path.join(savedir, confusion_filename)
+# plt.savefig(confusion_path)
+# plt.close()
+# # print(f"Confusion Matrix: \n{confusion}")
 
-comparison = "Malignant vs Benign"
+# comparison = "Malignant vs Benign"
 
-class_report = classification_report(y_test, predictions, target_names=["Malignant", "Benign"], digits=4, output_dict=True)
-class_report_df = pd.DataFrame(class_report).transpose()
+# class_report = classification_report(y_test, predictions, target_names=["Malignant", "Benign"], digits=4, output_dict=True)
+# class_report_df = pd.DataFrame(class_report).transpose()
 
-report_html = class_report_df.to_html()
+# report_html = class_report_df.to_html()
 
-html_sections = []
-html_sections.append(f"""
-                <h3>Confusion Matrix</h2>
-                <img src="{confusion_filename}" width="600">
-                <h3> Accuracy Table </h3>
-                {report_html}
-                     """)
+# html_sections = []
+# html_sections.append(f"""
+#                 <h3>Confusion Matrix</h2>
+#                 <img src="{confusion_filename}" width="600">
+#                 <h3> Accuracy Table </h3>
+#                 {report_html}
+#                      """)
 
-html_template = Template("""
-        <!DOCTYPE html>
-        <html lang="en">
-            <head>
-                <title> TabPFN Binary Breast Cancer Classification Report </title>
-            </head>
-            <body>
-                <h1>TabPFN Binary Breast Cancer Classification Report</h1>
-                <h2>Performance Matrix</h1>
-                {% for section in sections %}
-                {{section}}
-                {% endfor %}
+# html_template = Template("""
+#         <!DOCTYPE html>
+#         <html lang="en">
+#             <head>
+#                 <title> TabPFN Binary Breast Cancer Classification Report </title>
+#             </head>
+#             <body>
+#                 <h1>TabPFN Binary Breast Cancer Classification Report</h1>
+#                 <h2>Performance Matrix</h1>
+#                 {% for section in sections %}
+#                 {{section}}
+#                 {% endfor %}
 
-            </body>
+#             </body>
                          
-                         """)
+#                          """)
 
-html_output = html_template.render(
-    sections = html_sections,
-)
+# html_output = html_template.render(
+#     sections = html_sections,
+# )
 
-html_filename = savedir + "tabpfn_breast_cancer_classification_report.html"
-with open(html_filename, "w") as f:
-    f.write(html_output)
+# html_filename = savedir + "tabpfn_breast_cancer_classification_report.html"
+# with open(html_filename, "w") as f:
+#     f.write(html_output)
